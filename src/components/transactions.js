d24685480b733199c96aba1d4e33c2326424f537
Sanwal Khan <sanwal.awan.567@gmail.com>
2021-04-28 16:40:57 +0500

    updated transaction.js
    added classes i

--- src/components/transactions.js
+++ src/components/transactions.js
@@ -8,7 +8,7 @@ function Transaction({transaction}) {
 
 
   return (
-    <li className="minus">
+    <li className={transaction.amount < 0 ? "minus" : "plus" }>
       {transaction.description}
       <span>{Sign}${Math.abs(transaction.amount)}</span>
       <button className="delete-btn">X</button>
