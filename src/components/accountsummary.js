6f27c5be2382e480a557e96b015c09e2ecc9fa9e
Sanwal Khan <sanwal.awan.567@gmail.com>
2021-04-28 17:31:38 +0500

    Update accountsummary.js

--- src/components/accountsummary.js
+++ src/components/accountsummary.js
@@ -6,17 +6,17 @@ function AccountSummary() {
 
   const {transactions} = useContext(GlobalContext)
 
-  const amounts = transactions.map(transaction => transaction.amount);
+  const transactionamount = transactions.map(transaction => transaction.amount);
 
 
-  const income = amounts
-  .filter(item => item > 0)
-  .reduce((acc , item ) => (acc += item) , 0)
+  const income = transactionamount
+  .filter(transaction => transaction > 0)
+  .reduce((acc , transaction ) => (acc += transaction) , 0)
   .toFixed(2)
   
-  const expense = (
-    amounts.filter(item=> item < 0).reduce((acc, item) => (acc += item) , 0) * -1
-  ). toFixed(2)
+  const expense = Math.abs(
+    transactionamount.filter(transaction=> transaction < 0).reduce((acc, transaction) => (acc += transaction) , 0)
+  ).toFixed(2)
 
 
   return (
